6ae0433450407144ff0f0c97c114f202d55c0448
Aldrich <Aldrich@75.102.87.45>
2014-11-18 13:04:01 -0500

    Created basic application classes

---
+++ D4D/src/D4D/Car.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package D4D;
+
+import java.awt.Rectangle;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Aldrich
+ */
+public class Car extends Rectangle{
+    ImageIcon carType;
+}
---
+++ D4D/src/D4D/SettingPanel.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package D4D;
+
+import java.awt.LayoutManager;
+import java.awt.Rectangle;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Aldrich
+ */
+public class SettingPanel extends JPanel{
+    
+    int difficulty;
+    Rectangle car;
+    LayoutManager layout;
+    
+    public SettingPanel(){
+        
+    }
+}
